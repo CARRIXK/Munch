fe652ad6c3253a0986826f975029a2a290f1fb7b
Carrick <carrick.entwistle2001@gmail.com>
2025-03-26 13:34:30 +0000

    update Procfile and WSGI configuration

--- munch_project/settings.py
+++ munch_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-default-dev-key-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
